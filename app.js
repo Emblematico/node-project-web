fd2523bcba66eed2a642eba676ef260730151843
Alex Lopes <lopees.alex14@gmail.com>
2024-02-28 10:39:41 -0300

    tarefa aula 28/02/2024

--- app.js
+++ app.js
@@ -6,11 +6,15 @@ app.listen(8081, () => {
 });
 
 app.get('/', (req, res) => {
-    res.send('Oii, mundo!');
+    res.send('Home');
 });
 
-app.get('/cadastrar/:item/:quantidade', (req, res) => {
-    res.send('Pag de cadastro - Item: ' + req.params.item + ' - Quantidade: ' + req.params.quantidade);
+app.get('/produtos/:automovel/:marca/:modelo/:ano', (req, res) => {
+    res.send('Pag de produtos - Automovel: ' + req.params.automovel + ' - Marca: ' + req.params.marca + ' - Modelo: ' + req.params.modelo + ' - Ano: ' + req.params.ano);
+});
+
+app.get('/cadastrar/:usuario', (req, res) => {
+    res.send('Pag de cadastrar - Usuario: ' + req.params.usuario);
 });
 
 app.get('/contato', (req, res) => {
